f982ac5b53d114a2e805553010bdd2ce3e78778e
Chris Antonie Pieterse <Chrisantoniep@gmail.com>
2019-06-23 13:55:15 +0200

    Introduce usingtasks

---
+++ Chapter1/ImplementingMultithreadingAsynchronousProcessing/UsingTasks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter1.ImplementingMultithreadingAsynchronousProcessing
+{
+    class UsingTasks
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}

20b1c1bb98adab1c46a919cb8cc398eeb1db48fa
quantonie <antonie@quantsolutions.co.za>
2019-07-08 11:38:13 +0200

    Use Parallel class

--- Chapter1/ImplementingMultithreadingAsynchronousProcessing/UsingTheParallelClass.cs
+++ Chapter1/ImplementingMultithreadingAsynchronousProcessing/UsingTheParallelClass.cs
@@ -9,7 +9,42 @@ namespace Chapter1.ImplementingMultithreadingAsynchronousProcessing
     {
         public static void Run()
         {
-            Console.WriteLine("Hello World");
+            UsingParallelFor();
+            UsingParallelForeach();
+            UsingParallelBreak();
+        }
+
+        private static void UsingParallelFor()
+        {
+            Parallel.For(0, 10, value =>
+            {
+                Console.WriteLine($"Parallel for loop: {value}");
+                Thread.Sleep(1000);
+            });
+        }
+
+        private static void UsingParallelForeach()
+        {
+            var numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers, value =>
+            {
+                Console.WriteLine($"Parallel foreach loop: {value}");
+                Thread.Sleep(1000);
+            });
+        }
+
+        private static void UsingParallelBreak()
+        {
+            ParallelLoopResult result = Parallel.For(0, 10, (int value, ParallelLoopState loopState) =>
+            {
+                Console.WriteLine($"Parallel loop value: {value}; state: {loopState}");
+                if (value == 5)
+                {
+                    Console.WriteLine("Breaking Loop");
+                    loopState.Break();
+                }
+                return;
+            });
         }
     }
 }
